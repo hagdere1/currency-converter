2b3dcce624302830ef15acd15806fad3147499f1
Haik Gregory Agdere <hagdere1@binghamton.edu>
2016-06-26 22:33:11 -0400

    Style currency converter.

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -10,7 +10,7 @@ var App = React.createClass({
       amount: "",
       from: "USD",
       to: "USD",
-      convertedAmount: null
+      value: null
     };
   },
 
@@ -26,7 +26,6 @@ var App = React.createClass({
   onChange: function () {
     this.setState({ rates: RateStore.all() });
     if (this.state.rates.length === 0 || this.ratesOutdated()) {
-      debugger
       Util.updateExchangeRates();
     }
   },
@@ -47,6 +46,7 @@ var App = React.createClass({
     var currentDate = new Date(),
         previousDate = new Date(this.state.rates[0].created_at);
 
+    // Fetch new dates from Fixer API if local rates more than a day old
     return Math.floor((currentDate - previousDate) / 1000 / 60 / 60) >= 24;
   },
 
@@ -55,16 +55,18 @@ var App = React.createClass({
     // Set from and to rates to USD by default
     var fromRate = 1,
         toRate = 1,
-        amount = this.state.amount;
-debugger
+        amount = this.state.amount,
+        value;
+
     if (amount === "") {
-      this.setState({ convertedAmount: 0, amount: "" });
+      this.setState({ value: 0, amount: "" });
       return;
-    } else if (isNaN(amount) || amount < 0) {
-      this.setState({ convertedAmount: "Please enter a valid number.", amount: "" });
+    } else if (amount < 0) {
+      this.setState({ value: "Cannot enter a negative value.", amount: "" });
       return;
     }
 
+    // Find rates corresponing to the selected currencies
     this.state.rates.forEach(function (rate) {
       if (rate.currency === this.state.from) {
         fromRate = parseFloat(rate.rate);
@@ -73,15 +75,18 @@ debugger
       }
     }.bind(this));
 
-    this.setState({ convertedAmount: (1 / fromRate) * parseFloat(amount) * toRate });
+    value = this.state.amount + " " + this.state.from + " " + "=" + " " +
+            ((1 / fromRate) * amount * toRate).toFixed(2) + " " + this.state.to;
+
+    this.setState({ value: value });
   },
 
   render: function () {
-    var convertedAmount;
-    if (this.state.convertedAmount) {
-      convertedAmount = this.state.convertedAmount;
+    var value;
+    if (this.state.value) {
+      value = <div id="value">{this.state.value}</div>;
     } else {
-      convertedAmount = <div></div>;
+      value = <div id="value">-</div>;
     }
 
     var fromCurrencies = RateStore.all().map(function (fromRate) {
@@ -93,28 +98,35 @@ debugger
     }.bind(this));
 
     return (
-      <div>
-        <h1>Currency Calculator</h1>
-        <form onSubmit={this.calculate}>
-
-          <label>From</label>
-          <select onChange={this.setFromCurrency} value={this.state.from}>
-            <option value="USD">USD</option>;
-            {fromCurrencies}
-          </select>
-
-          <label>To</label>
-          <select onChange={this.setToCurrency} value={this.state.to}>
-            <option value="USD">USD</option>;
-            {toCurrencies}
-          </select>
-
-          <input type="text" onChange={this.setAmount} value={this.state.amount} />
-
-          <button>Convert</button>
-        </form>
-
-        {convertedAmount}
+      <div id="container">
+        <div id="header_banner">
+          <h1>Currency Converter</h1>
+        </div>
+        <div id="calculator">
+          <form className="group" onSubmit={this.calculate}>
+            <div className="dropdown">
+              <label>From</label>
+              <select id="from_currency" onChange={this.setFromCurrency} value={this.state.from}>
+                <option value="USD">USD</option>;
+                  {fromCurrencies}
+              </select>
+            </div>
+            <div className="dropdown">
+              <label>To</label>
+              <select id="to_currency" onChange={this.setToCurrency} value={this.state.to}>
+                <option value="USD">USD</option>;
+                {toCurrencies}
+              </select>
+            </div>
+            <input type="number"
+                   onChange={this.setAmount}
+                   value={this.state.amount}
+                   placeholder="Amount"
+                   step="any"/>
+            <button>Convert</button>
+          </form>
+          {value}
+        </div>
       </div>
     );
   }
